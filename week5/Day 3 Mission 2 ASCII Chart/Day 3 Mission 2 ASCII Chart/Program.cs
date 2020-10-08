ea125ef4d493a0c8030b471096e261538f76b003
AnnekeLisa <71494183+AnnekeLisa@users.noreply.github.com>
2020-10-08 09:31:46 +0200

    ASCII Chart

---
+++ week5/Day 3 Mission 2 ASCII Chart/Day 3 Mission 2 ASCII Chart/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Day_3_Mission_2_ASCII_Chart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int asciiCharakterNumber = 0;
+            int decimalNumber = 0;
+
+            for (int i = 0; i < 250; i++)
+            {
+                char asciicharacter = (char)asciiCharakterNumber;
+
+                Console.WriteLine($"{decimalNumber} = {asciicharacter}");
+
+                asciiCharakterNumber++;
+                decimalNumber++;
+            }
+        }
+    }
+}
