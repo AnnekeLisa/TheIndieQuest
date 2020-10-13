81461658beaeada9e042a4d78b223cc4fa35b34a
AnnekeLisa <71494183+AnnekeLisa@users.noreply.github.com>
2020-10-13 17:01:14 +0200

    mission 2 dice is standard method

--- week5/W5D4M1 Dice String Split/W5D4M1 Dice String Split/Program.cs
+++ week5/W5D4M1 Dice String Split/W5D4M1 Dice String Split/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace W5D4M1
 {
@@ -7,12 +8,87 @@ namespace W5D4M1
         static void Main(string[] args)
         {
             Console.WriteLine($"{StandardRoll("d6")} ");
+            IsStandardDiceNotation("d6");
+            
             Console.WriteLine($"{StandardRoll("3d6+12")} ");
-            Console.WriteLine($"{StandardRoll("3d6-5")} ");
-            Console.WriteLine($"{StandardRoll("2d4")} ");
+            IsStandardDiceNotation("3d6+12");
+
+            Console.WriteLine($"Rolling 3d6*12");
+            IsStandardDiceNotation("3d6*12");
+
+            Console.WriteLine($"Rolling 34");
+            IsStandardDiceNotation("34");
+
+           // Console.WriteLine($"{StandardRoll("3d6-5")} ");
+           // Console.WriteLine($"{StandardRoll("2d4")} ");
         }
 
-        
+        static bool IsStandardDiceNotation(string text)
+        {
+            bool isStandard = false;
+            bool containsInt = text.Any(char.IsDigit);
+
+            foreach (char check in text)
+            {
+                if (check == 'd')
+                {
+                    isStandard = true;
+                }
+            }
+
+            if (containsInt == false)
+            {
+                isStandard = false;
+            }
+
+            if (!isValidUsername(text))
+            {
+                isStandard = false;
+            }
+
+            if (isStandard)
+            {
+                Console.WriteLine($"Rolling {text} is valid");
+            }
+            if (!isStandard)
+            {
+                Console.WriteLine($"Cannot roll {text}. Thats not standard dice notation.");
+            }
+
+
+
+
+            return isStandard;
+        }
+
+        static bool isValidUsername(string name)
+        {
+            bool validUsername = true;
+            string specialChar = @"\\|_!#$%&/()*=?»«@£§€{}.-;'<>_,";
+
+            foreach (char c in name)
+            {
+                if (System.Char.IsUpper(c))
+                {
+                    validUsername = false;
+                }
+
+                /*if (System.Char.IsDigit(c))
+                {
+                    validUsername = true;
+                }*/
+
+                foreach (char special in specialChar)
+                {
+                    if (c == special)
+                    {
+                        validUsername = false;
+                    }
+                }
+            }
+            return validUsername;
+        }
+
         static int DiceRoll(int numberOfRolls, int diceSides, int fixedBonus = 0)
         {
             int diceRoll = 0;
