1ba47a88e01137cc96b8ee74aaf110c19ba84fdd
AnnekeLisa <71494183+AnnekeLisa@users.noreply.github.com>
2020-10-19 16:52:32 +0200

    recursive methods

---
+++ week3/RecursiveMethods/RecursiveMethods/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace RecursiveMethods
+{
+    class Program
+    {
+        static int Factorial(int n)
+        {
+            //[n × (n - 1) × (n - 2) × … × 1]
+
+            if (n == 0)
+            {
+                return 1;
+            }
+            else 
+            {
+                return n * Factorial(n-1);
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            var random = new Random();
+            int magicNumber = random.Next(0,11);
+
+            Console.Write($"{magicNumber}! = ");
+            
+            Console.WriteLine(Factorial(magicNumber));
+
+
+        }
+    }
+}
